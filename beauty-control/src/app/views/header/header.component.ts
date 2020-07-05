ecdfc84db239dd249a9afa877049ad7c894445f4
Harã Heique dos Santos <heikacademicos@hotmail.com>
2020-07-05 05:40:06 -0300

    feat: adicionando icone de usuário na header

--- beauty-control/src/app/views/header/header.component.ts
+++ beauty-control/src/app/views/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import * as M from 'materialize-css';
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   logoSrc: string = "assets/logos/icon-beautycontrol-white.png";
 
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(function () {
+      const elem: Element = document.getElementById("side-navbar");
+      M.Sidenav.init(elem);
+    }, 0);
   }
 }
