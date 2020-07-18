b90335ab2c0b9268d232a312858c00670356a690
Harã Heique dos Santos <heikacademicos@hotmail.com>
2020-07-18 09:54:29 -0300

    feat: customização da barra lateral e do navbar padrão

--- beauty-control/src/app/views/header/header.component.ts
+++ beauty-control/src/app/views/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
 import * as M from "materialize-css";
 
 @Component({
@@ -9,17 +15,15 @@ import * as M from "materialize-css";
 export class HeaderComponent implements OnInit, AfterViewInit {
   logoSrc: string = "assets/logos/icon-beautycontrol-white.png";
 
-  @ViewChild('sidenav') sidenavElem: ElementRef;
-  @ViewChild('dropdown') dropdownElem: ElementRef;
+  @ViewChild("sidenav") sidenavElem: ElementRef;
+  @ViewChild("dropdown") dropdownElem: ElementRef;
 
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     //new M.Sidenav(this.sidenavElem.nativeElement, {});
     M.Sidenav.init(this.sidenavElem.nativeElement, {});
-    M.Dropdown.init(this.dropdownElem.nativeElement, {});
   }
 }
