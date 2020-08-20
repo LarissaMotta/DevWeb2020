d6597935508599e2a61f5b06bac9b3215e16af98
Harã Heique dos Santos <heikacademicos@hotmail.com>
2020-08-20 04:12:30 -0300

    feat: adicionando primeNG

--- beauty-control/src/app/app.module.ts
+++ beauty-control/src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { PrimengModule } from './modules/primeng.module';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './views/header/header.component';
-import { FooterComponent } from './views/footer/footer.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./views/header/header.component";
+import { FooterComponent } from "./views/footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
+    AppComponent, 
+    HeaderComponent, 
+    FooterComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    AppRoutingModule,
+    PrimengModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
