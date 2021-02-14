7f32b5a677d10a0f93469a75309517a0a6eb2e56
HaraHeique <heikacademicos@hotmail.com>
2021-02-14 07:25:05 -0300

    feat: adição do loader ao submeter login

--- beauty-control/src/app/views/login/login.component.ts
+++ beauty-control/src/app/views/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from "@angular/router";
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { AuthService } from "src/app/services/auth.service";
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: "app-login",
@@ -10,20 +11,22 @@ import { AuthService } from "src/app/services/auth.service";
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  urlImg: string;
+	urlImg: string;
   formLogin: FormGroup;
   formSubmitted: boolean;
 	invalidUserSubmit: boolean;
+	isSubmittingAuth: boolean;
 	
 	subscriptions: Subscription;
 
   constructor(private router: Router, private authService: AuthService) {
 		this.subscriptions = new Subscription();
-    this.redirectToHome();
+		this.redirectToHome();
     this.urlImg = "assets/logos/icon-white-fundo-transparente.png";
     this.formLogin = this.createForm();
     this.formSubmitted = false;
 		this.invalidUserSubmit = false;
+		this.isSubmittingAuth = false;
   }
 	
 	ngOnInit(): void {}
@@ -39,24 +42,27 @@ export class LoginComponent implements OnInit, OnDestroy {
   isFormFieldInvalid(field: string): boolean {
     return (
       (!this.formLogin.get(field).valid && this.formLogin.get(field).touched) ||
-      (this.formLogin.get(field).untouched && this.formSubmitted) ||
       this.invalidUserSubmit
     );
   }
 
   submitAuth(): void {
-    this.formSubmitted = true;
+		this.formSubmitted = true;
 
     if (this.formLogin.valid) {
+			this.isSubmittingAuth = true;
+
 			this.subscriptions.add(
 				this.authService.login(this.formLogin.value.email, this.formLogin.value.password)
 					.subscribe({ 
-						error: () => this.invalidUserSubmit = true
-					})
+						error: () => { 
+							this.invalidUserSubmit = true;
+						}
+					}).add(() => this.isSubmittingAuth = false)
 			);
     } else {
       this.formLogin.markAllAsTouched();
-    }
+		}
   }
 
   private createForm(): FormGroup {
